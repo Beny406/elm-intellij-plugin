a2ed4e446098aeca4de8e312591451e5e8a92cc3
GitHub Action <action@github.com>
2024-02-20 11:02:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/beny406/elmintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/beny406/elmintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.beny406.elmintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/beny406/elmintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/beny406/elmintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.beny406.elmintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
